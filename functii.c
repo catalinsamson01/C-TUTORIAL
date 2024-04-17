08cc3d5eedab0c9e618dee752cbf6f5bf94fd058
catal <catalin16mai@yahoo.com>
2024-04-17 23:04:23 +0300

    tutorials

--- functii.c
+++ functii.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
 
-int main(){
-
-int a = 10;
-float b = 20.5;
-char c = 'C';
-
-if (a > 5)
+void birthday()
 {
-    printf("a este mai mare decat 5\n");
-} else
-    printf("a este mai mic decat 5\n");
-
-
-
-    for (int i = 0; i < 5; i++)
-    {
-        printf("%d ", i);
-    }
-    
-    return 0;
-    
+   printf("\nHappy birthday to you!");
+   printf("\nHappy birthday to you!");
+   printf("\nHappy birthday dear...YOU!");
+   printf("\nHappy birthday to you!\n");
 }
+
+int main()
+{
+   birthday();
+   birthday();
+   birthday();
+
+   return 0;
+}
+
 
