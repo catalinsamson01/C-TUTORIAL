57a62ae0a09922d4a0e9a71f313ffb827ba88677
catal <catalin16mai@yahoo.com>
2024-04-17 21:34:55 +0300

    first commit

---
+++ HelloWorld.c
@@ -0,0 +1,19 @@
+#include<stdio.h> 
+int main()
+{
+// this is a comment
+    int x=10, y=20;
+    printf("%d\n",x+y);
+    printf("Hello world!");
+    printf("Salut ba");
+
+    return 0;
+
+
+
+ 
+}
+
+
+
+
---
+++ functii.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+
+int a = 10;
+float b = 20.5;
+char c = 'C';
+
+if (a > 5)
+{
+    printf("a este mai mare decat 5\n");
+} else
+    printf("a este mai mic decat 5\n");
+
+
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d ", i);
+    }
+    
+    return 0;
+    
+}
+
